d65098b89727a2893873f9f4e08ba1b94731abc8
Zhouzhou Du <john_1990@163.com>
2021-11-25 23:40:55 +0800

    add user data and local docker-compose override and fix local run UI proxy server side port

---
+++ infra/src/aws-training-ec2-user-data.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install docker
+service docker start
+usermod -a -G docker ec2-user
+chkconfig docker on
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo 'Start creating deployment script...'
+rm -rf /home/ec2-user/deploy.sh
+cat > '/home/ec2-user/deploy.sh' <<- EOM
+#!/bin/bash
+
+`aws ecr get-login --no-include-email --region ap-east-1`
+docker-compose rm -f
+docker-compose pull
+docker-compose up -d
+EOM
+
+sudo chmod 755 /home/ec2-user/deploy.sh
+
+echo 'Start creating docker compose script...'
+rm -rf /home/ec2-user/docker-compose.yml
+cat > '/home/ec2-user/docker-compose.yml' <<- EOM
+version: "3.9"
+services:
+  web:
+    image: xxxxxx
+    tty: true
+    user: root
+    ports:
+      - "8080:8080"
+EOM
+
+sudo chmod 755 /home/ec2-user/docker-compose.yml
